b4e36822b3e71ec8b300f47c9413f4ad107aef31
heesak <adesuyiojo04@gmail.com>
2022-08-08 14:30:18 +0100

    our ser

---
+++ main.js
@@ -0,0 +1,9 @@
+// document.getElementById('orangeiv')
+var secondiv = document.getElementById('orangediv')
+// poping on alertwhen clicked
+secondiv.addEventListener('click', function(){
+    alert('orange div is clicked')
+})
+// changing the elment background
+secondiv.style.background="blue"
+
